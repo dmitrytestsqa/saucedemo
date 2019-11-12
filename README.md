6b68a05e6502276d008f9347c3687bd1b9275359
Dmitry Antonov <dmitrytestsqa@gmail.com>
2019-11-12 17:36:05 +0300

    readme.md

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # saucedemo
+In this project you can see UI atomated tests which are use Selenium and JUnit. Also project was performed with Page Object Pattern.
--- src/test/java/saucedemo/pages/Finish.java
+++ src/test/java/saucedemo/pages/Finish.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 public class Finish {
 
-  WebDriver driver;
+  public WebDriver driver;
 
   public Finish(WebDriver driver) {
     this.driver = driver;
